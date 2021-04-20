eaf2e9a76f094113dde8d7b5a03b9450ddb66c38
Pyami <1pyami@gmail.com>
2021-04-20 15:29:32 +0530

    completes js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const input = document.getElementById('input');
 const btn = document.getElementById('generateBtn');
 
 
-btn.addEventListener('click', generation())
+btn.addEventListener('click', generation)
 
 function generation() {
     let randomNumber = Math.floor(10000000 + Math.random() * 90000000)
